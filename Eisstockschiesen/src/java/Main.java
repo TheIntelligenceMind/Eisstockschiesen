06a7526135134ed81fbd0e10f6af1480efd5951a
TheIntelligenceMind <t.hermbecker@live.de>
2017-01-03 21:43:01 +0100

    KKK

--- Eisstockschiesen/src/java/Main.java
+++ Eisstockschiesen/src/java/Main.java
@@ -6,5 +6,9 @@ public class Main {
 		System.out.println("Test");
 		System.out.println("Test");
 		System.out.println("Test");
+		
+		
+		System.out.println("Test");
+		System.out.println("Test");
 	}
 }
